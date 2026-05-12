c4418a9b9496d78352768b418018d27866431771
Tuba282 <tubajan282@gmail.com>
2026-05-12 12:27:51 +0500

    JavaScript start

---
+++ JavaScript/class-01/app.js
@@ -0,0 +1,62 @@
+// console.log("Maryam");
+// console.log("Noori");
+// exceution -- interpreter
+
+// commenting JS
+
+// single line comment
+
+/*------*/
+
+/*console.log("Maryam");
+console.log("Noori");*/
+
+// console.log("Noori");
+// console.log("Yumna");
+// console.log("madiha");
+
+//  devlope -- testing - tool - console
+
+// button --add function /sction - testing - tools -  console
+// console - testing - debugging - execution (output dekhna)
+
+
+// var -- variables create karne
+// ; - terminator
+
+// var VARIABLE ; // declaration
+// VARIABLE = "Ayesha"; //initialization
+// VARIABLE = 24; //reassigning
+// console.log(VARIABLE);
+
+
+// acbhy#$%^ --- String -- "abc" 'abc' "'
+// 2587 --- Number --- 24 58 78
+
+// var name = "Ayesha";
+// console.log(name);
+
+// name = "Tuba";// reassignment
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+
+// var story = 'My mother told me this and this this'
+// console.log("My mother told me this and this this")
+// console.log(story);
+
+
+
+// JS - case sensitive 
+// -- "A" not equal to "a"
+//  -- Alia - alia 
+// var -- var - output 
+// Var -- error
+
+// Var name = " Tuba" -- error
+// 
+// var continue = "continue";
+// console.log("24");
