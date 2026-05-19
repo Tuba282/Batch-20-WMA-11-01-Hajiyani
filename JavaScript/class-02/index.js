1ea6f8b2329f4f43cf9881f927544a47966fbd0f
Tuba282 <tubajan282@gmail.com>
2026-05-19 13:01:49 +0500

    alert + variable names(legal/ilegal) + convensions + math operations + concatination

---
+++ JavaScript/class-02/index.js
@@ -0,0 +1,93 @@
+// var --variable create keyword
+// alert("Hello World !! line -2 ")
+// window.alert("Hello Class !!  line -3 ")
+// console.log("Class 02 !!  line -4 ")
+
+// variables for string - Number
+// var name = "Madiha1234!@$%/*-+"
+// anything enclosed in "" '' that is string
+// console.log(name);
+// var phone = 3456999;
+// console.log(phone + 3);//3456 - 3459
+
+// ----- Variables Name legal-illegal
+
+// charcters , numbers , _ , $
+
+var myVariavle;
+var myVar1;
+var myVar20;
+var myVariable_;
+var _myVar;
+var $myVar_;
+var My$Var2_;
+var rollNo ;
+var A123;
+var propertyName;
+
+// ilegal variable names
+// spaces , number at the start , other special charcters
+
+// var my variable ;
+// var 1ilegalVar;
+// var mayVariable@;
+// var mayVariable*;
+// var 123 ="string";
+
+
+
+// Variable Convensions .
+
+// - Camel Case
+// i.e camelCase - propertName - studentIdCard
+// i.e myNameIsTubaJanAndIAmATrainer
+
+// - Pascal Case 
+// i.e ForExample - AnyQuestion
+// i.e ILiveInKarachi
+
+// - Snake Case
+// i.e pick_mobile - doing_work - property_name
+// i.e my_name_is_madiha
+
+
+// Math expressions:
+// Familiar operators ( + - * / % )
+
+var num_1 = 12;
+var num_2 = 10;
+
+// console.log(num_1 + num_2); // 22
+// console.log(num_1 + num_2 + 22); // 44
+// console.log(num_1 - num_2); // 2
+// console.log(num_1 * num_2); // 120
+// console.log(num_1 / num_2); // 120
+// console.log(12 % 10); // 2
+
+console.log(12 + "5");//125 - concatinate 17 - 20
+console.log( "5" + 12);//512 - concatinate 17 - 20
+console.log( "2" + "12");//212 - concatinate 17 - 20
+// console.log(num_1 + "5");//125 - 17 - 20
+console.log(num_1 - "5");//7 - number
+console.log(num_1 * "5");//60 - number
+console.log(num_1 / "5");//2.4 - number
+console.log(num_1 % "5");//2 - number
+
+// -- Data Types
+
+// JS mainly 2 types - 8 dataTypes (2 Categories)
+
+// Primitive DataType - direct value - (unmutable) - notChange
+// String -- ""
+// Number -- 1234  
+// Boolean -- true/false
+// undefined 
+// null      -- (null !== 0)
+// Symbol()
+// Non-Primitive DataType - reference - (mutable) - changes possible
+// object
+// Array
+
+var location = undefined;
+var isPresent = false;
+
