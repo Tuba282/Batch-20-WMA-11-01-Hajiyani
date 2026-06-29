79ea2385fa4afc8c3c809829fc69548b59242576
Tuba282 <tubajan282@gmail.com>
2026-06-30 04:47:43 +0500

    bushra commit

--- JavaScript/class-11/app.js
+++ JavaScript/class-11/app.js
@@ -1,122 +1,18 @@
-/*
- ----for loop -------- counter loop , exact count
-
-syntax
-
-for(condition){
-    body...
-    }
-
-    for (i = 0; i <= 10; i++){
-    console.log(i);
-    }
-    */
-
-
-
-// i++ updation / increment
-// i-- decrement
-
-
-//---1----10---console
-// for (i = 0; i <= 10; i++) {
-//     console.log(i);
-// }
-
-
-// array ka count agr dena hy to ) sy start ho ga
-// for (let x = 0; x < 10; x++) {
-//     console.log(x)
-// }
-
-
-// ui
-// for (i = 0; i <= 10; i++) {
-//   document.writeln(i)
-// }
-
-
-
-// for (let index = 0; index <= 10; index++) {
-//     console.log(index);
-// }
-
-
-
-// ---variable ky sath for loop
-// var count = 50;
-// for (i = 20; i <= count; i++){
-//     console.log(i)
-// }
-
-
-// -----i+=1 ---i++----
-//-----variable ky through count lia hy for loop ka 
-// var countStart = 0;
-// var countEnd = 20;
-// for (i = countStart; i <= countEnd ; i+=1){
-//     console.log(i)
-// }
-
-
-//user input
-// var Start = +prompt("Enter Start Number: ");
-// var End = +prompt("Enter End Number : ")
-
-// for (let i = Start; i <= End; i++){
-//     console.log(i);
-// }
-
-
-
-// for (let i = Start; i <= End; i++) {
-//     document.writeln("<br> " + i);
-// }
-
-
-// for (let i = 0; i <= 34; i++) {
-//     document.writeln("<br> " + i);
-// }
-
-// ---------------------------------
-
-// var tableStart = +prompt("Enter Start Number");
-// var tableEnd   = +prompt("Enter End Number");
-
-// for (let i = tableStart; i<= tableEnd; i++  ){
-//     console.log(tableStart + "x" + i + "=" + (tableStart * i ));
-// }
-
-
-//----------------------------
-// for (let i = 1; i<= tableEnd; i++  ){
-//     // 2 x 1 = 2
-//     console.log(tableStart + " x " + i + " = " + (tableStart * i ));
-// }
-
-// ------------------
-// var array = ["A", "B", "C"]
-//var array = ["Ayesha", "Bashair", "Cola"]
-
-// for (let i = 0; i <= 2; i++) {
-//     console.log(array[i] + " " + i)
-// }
-
-
 // user input array with flag concept or break
 // var array = ["Karachi", "Lahore", "Islamabad"]
 // var city = prompt("Enter City : ")
 // var flag = false;
 
 // for (let i = 0; i < array.length; i++) {
-//     if (city === "Karachi") {
-//         console.log(i  + " City name : " + city)
+//     if (city === array[i]) {
 //         flag = true;
+//         console.log(i + " Index  of  City name : " + city)
 //         break;
 //     }
-//     else {
-//         console.log("nhi mila ....")
-//     }
+// }
+
+// if (flag == false) {
+//     console.log("city nhi mila ....")
 // }
 
 
@@ -129,7 +25,7 @@ for(condition){
 // }
 
 
-/// nested problem hy theek krna hy 
+/// nested problem hy theek krna hy
 //-----------------------------------------
 // var array1 = [3, 5, 7, 8]
 // var array2 = ["A", "B", "C", "D"]
@@ -151,6 +47,220 @@ for(condition){
 //     }
 // }
 
+//------------------------------------------------------solution
+
+// var array1 = ["A", "B", "C", "D"]
+// var array2 = ["A", "B", "C", "D"]
+// var fullAray = []
+
+// for (let i = 0; i < array1.length; i++) {
+//     for (let j = 0; j < array2.length; j++) {
+
+//         if (i == j) {
+//             fullAray.push(array1[i] + " " + array2[j])
+//         }
+//     }
+// }
+// console.log(fullAray)
+
+//------------------------------------------------------------firstname with last name
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+// var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+// var fullNames = [];
+// for (var i = 0; i < firstNames.length; i++) {
+//     for (var j = 0; j < lastNames.length; j++) {
+
+//         if (i == j) {
+//             fullNames.push(firstNames[i] + lastNames[j]);
+//         }
+//     }
+// }
+// console.log(fullNames);
+
+// //output :-
+// ['BlueRay Zzz', 'Upchuck Burp', 'Lojack Dogbone', 'Gizmo Droop']
+
+
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+
+// while loop syntax
+// while (condition) {
+//     // Code to execute
+// }
+
+
+// Condition check hoti hai.
+// Agar condition true ho to code execute hota hai.
+// Phir dobara condition check hoti hai.
+// Jab condition false ho jaye to loop stop ho jata hai.
+
+// --------------------------------------------------------------
+// --------------------------------------------------------------
+
+// let i = 1;
+
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+// }
+
+// //output:-
+// 1
+// 2
+// 3
+// 4
+// 5
+
+
+// i ki value 1 hai.
+// Condition i <= 5 true hai.
+// 1 print hua.
+// i++ se value 2 ho gayi.
+// Ye process tab tak chalta raha jab tak i 6 nahi ho gaya.
+// 6 <= 5 false hua aur loop stop ho gaya.
+//---------------------------------------------
+//---------------------------------------------
+
+
+//Print Even Numbers
+
+// let num = 2;
+
+// while (num <= 10) {
+//     console.log(num);
+//     num += 2; // i+=2 hota hy jasy btaya tha class main , i++ ka back i+=1
+// }
+
+
+// //output:-
+// 2
+// 4
+// 6
+// 8
+// 10
+
+
+// num ki starting value 2 hai.
+// Har iteration ke baad 2 add hota hai.
+
+//--------------------------------------------------
+//--------------------------------------------------
+//--------------------------------------------------
+//--------------------------------------------------
+//--------------------------------------------------
+
+//----- do while syntax
+
+// do {
+//     // Code to execute
+// } while (condition);
+
+
+// Pehle code execute hota hai.
+// Uske baad condition check hoti hai.
+// Agar condition true ho to loop dobara chalta hai.
+// Agar condition false ho to loop stop ho jata hai.
+
+// Difference: while loop mein condition pehle check hoti hai, jabke do...while mein code pehle ek baar zaroor execute hota hai.
+
+
+//-----------------------------------------------------------
+//-----------------------------------------------------------
+
+// example 1 ------ Count 1 to 5
+
+// let i = 1;
+
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 5);  // constion check hogi yahn 
+
+// //output:-
+// 1
+// 2
+// 3
+// 4
+// 5
+
+// // i ki value 1 hai.
+// // do block pehle execute hota hai aur 1 print hota hai.
+// // i++ se value 2 ho jati hai.
+// // Condition 2 <= 5 true hai, isliye loop dobara chalta hai.
+// // Ye process 5 tak chalta hai.
+// // Jab i 6 hota hai to condition false ho jati hai aur loop stop ho jata hai.
 
 
 
+//------------------------------------------------------------------------------
+// let i = 1;
+// 
+// do {
+    // console.log(i);
+    // i++;
+// } while (i <= 5);
+// 
+// 1
+// 2
+// 3
+// 4
+// 5
+
+//----------------------------------------------------- -----------------------------------------------------
+
+//  While Loop                                       | Do While Loop                                        |
+//  ------------------------------------------------ | ---------------------------------------------------- |
+//  Condition pehle check hoti hai.                  | Code pehle execute hota hai.                         |
+//  0 ya us se zyada baar chal sakta hai.            | Kam az kam 1 baar zaroor chalta hai.                 |
+//  Agar condition false ho to kuch print nahi hota. | Condition false ho tab bhi ek baar execute hota hai. |
+//  Syntax: `while (condition) {}`                   | Syntax: `do {} while (condition);`                   |
+
+
+
+
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+
+// string methods
+// .toLowerCase() >>>>> ismain sab small abc main convert hota hy
+// .toUpperCase() >>>>> ismain sab big   abc main convert hota hy
+
+
+// var name1 = "BuShRa"
+// console.log(name1.toLowerCase());
+
+//-------output:-
+//bushra
+
+//--------------------------------------------------userinut
+// var name2 = prompt("Enter Name: ");
+// console.log(name2.toLowerCase());
+
+//-------output:-
+//HaDia as a prompt -----output hadia
+
+
+// ---------------------------------------------------------------------
+
+// var name3 = "Sadia Rihana"
+// console.log(name3.toUpperCase());
+
+//-------output:-
+//SADIA RIHANA
+
+//--------------------------------------------------userinut
+// var name4 = prompt("Enter Name: ");
+// console.log(name4.toUpperCase());
+
+//-------output:-
+//Hadiiixaxsa as a prompt -----output Hadiiixaxsa
+
+//---------------------------------------------------------------
+//---------------------------------------------------------------
+//---------------------------------------------------------------
+//---------------------------------------------------------------
+//---------------------------------------------------------------
