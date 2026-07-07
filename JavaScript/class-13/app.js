49c71fe8f88787954f8997aeb6864ca1b4696e74
Tuba282 <tubajan282@gmail.com>
2026-07-07 13:11:13 +0500

    revise quiz concepts ,Number methods

--- JavaScript/class-13/app.js
+++ JavaScript/class-13/app.js
@@ -1,4 +1,4 @@
-console.log("hello");
+// console.log("hello");
 
 
 //----------------- Syntax
@@ -26,9 +26,51 @@ console.log("hello");
 //login portal time 11:55
 
 
+// quiz time end 12:51
 
 
 
 
+//syntax   Math.ceil()   ismmain number berhta hy 
+//syntax   Math.floor()  number berhta nhi hy
+
+// var score1 = 3.4
+
+// console.log(Math.ceil(score1)); //4
 
 
+
+// var score1 = 3.6
+// console.log(Math.floor(score1)); //3
+
+
+
+
+// var number = -3.6
+
+// console.log(Math.floor(number)); //
+
+
+//--------------Math.random()
+//--------------------------------------------issue
+// var number = 10
+
+
+// number = Math.random()
+// console.log(number)  wrong way 
+
+
+//right way
+// var dice = (Math.floor(Math.random() * 10) + 1);
+// console.log(dice);
+
+
+//dice
+// var dice = (Math.floor(Math.random() * 6) + 1);
+// console.log(dice);
+
+
+//________________________________________________
+// 100
+// var dice = (Math.floor(Math.random() * 100) + 1);
+// console.log(dice);
