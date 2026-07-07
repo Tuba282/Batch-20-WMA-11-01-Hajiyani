6142edea9fbfe68cb2cddcbf8efb6067887ce59e
Tuba282 <tubajan282@gmail.com>
2026-07-07 11:58:10 +0500

    revise quiz concepts , round numbers

---
+++ JavaScript/class-13/app.js
@@ -0,0 +1,34 @@
+console.log("hello");
+
+
+//----------------- Syntax
+//Math.round();
+
+//----------3------
+
+// var score = 3.12469;
+
+// console.log(score);
+
+
+// console.log(Math.round(score)); // 3
+
+
+
+//var score2 = 0.6469;
+
+//console.log(Math.round(score2)); // 1
+
+
+//------------------ Quiz time ------------------------------
+
+//student are preparing for quiz ...
+//login portal time 11:55
+
+
+
+
+
+
+
+
