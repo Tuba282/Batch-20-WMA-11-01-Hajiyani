e07ddbf610405c4e57df8e9b393a719ad40e3661
Tuba282 <tubajan282@gmail.com>
2026-07-14 13:11:09 +0500

    Getting the current date and time , Extracting parts of the date and time ,Specifying a date and time, Changing elements of a date and time

---
+++ JavaScript/class-14/app.js
@@ -0,0 +1,125 @@
+// -------  string --  Number 
+
+var str = "78.987"
+// var convertToInt = parseInt(str)
+// console.log( str);
+// console.log( convertToInt);
+// console.log(typeof str);
+// console.log(typeof convertToInt); // point remove + number ,main convert 
+
+// console.log(parseFloat(str));
+//  floating num === decimal numbers
+
+
+// var userInput = prompt("Enter Your Ammount");
+// var tax = 200
+// console.log(parseInt(userInput) + tax);
+
+
+
+// -------  NUmber  --> Number() -- String ------------> .toString()
+
+
+
+// var convertToInt = Number(str)
+// console.log(typeof convertToInt);//Number
+
+
+
+// var num = 67.765;
+// console.log(num.toString() - 2);
+// console.log(typeof num.toString());
+
+
+
+
+// ---- Maths Function ------------ generate 6 digit random num ------------
+
+
+
+// var random = Math.floor((Math.random() * 100000) + 1)
+// console.log(random);
+// console.log(parseInt(random));
+
+
+
+// -------------- Date Object In JS ---------------
+
+
+var now = new Date()
+
+// var today = now.getDate()
+// var day = now.getDay()
+
+
+// var days = ['Sunday' ,'Monday', 'Tuesday', 'Wednesday', 'Thrusday', 'Friday', 'Saturday']
+// console.log(today);
+// console.log(days[now.getDay()]);
+
+
+// console.log("Year " + now.getFullYear());
+// console.log("Hours " + now.getHours());
+// console.log("Minutes " + now.getMinutes());
+// console.log("Seconds " + now.getSeconds());
+// console.log("Milliseconds " + now.getMilliseconds());
+// console.log(now.getTime());
+
+
+
+//12:AM 00:00
+// 11:00 PM -- 23:00 
+
+
+// -- ------------------While Loop-------------
+
+
+
+
+
+
+// for (initialization; condition ; update){
+// Block of code
+// }
+
+
+// while loop----- syntax 
+
+// initialization
+
+
+// while(condition){
+// block of code
+// block of code
+// block of code
+// block of code
+
+
+// update
+// }
+
+
+// var table = prompt("Enter your Number !!")
+
+// var i = 1;
+
+// if (typeof table == 'number') {
+//     for (var i = 1; i <= 10; i++) {
+//         document.write(`<p>${table} x ${i} =  ${table * i}</p>`)
+//     }
+// } else {
+//     alert("Invalid Input")
+// }
+
+// console.log(typeof table == 'number');
+
+
+// -------------------  document.write ---------------
+
+// var table = prompt("Enter your Number !!")
+
+// for (var i = 1; i <= 10; i++) {
+//     document.write(`<p>${table} x ${i} =  ${table * i}</p>`)
+// }
+
+
+// ----- while Loop ---------
---
+++ JavaScript/class-15/app.js
@@ -0,0 +1,166 @@
+// date and time
+
+
+// var DateNow = new Date();
+
+// console.log(DateNow);
+
+
+//days------------------------------------------------------
+
+// var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+// var now = new Date();
+
+// var theDay = now.getDay();  //number 
+
+// console.log(theDay);
+
+//  var nameOfToday = dayNames[theDay];
+
+//  console.log(nameOfToday);
+
+//-----------------------------------------month
+
+// var now = new Date();
+
+// var month = ["January", "February", "March", "April", "May", "June",   "July", "August", "September", "October", "November","December" ]
+
+// var currentMonth = now.getMonth(); 
+
+// // console.log(currentMonth); //6 as a number 
+
+
+// var NameofMonth = month[currentMonth] 
+
+
+// console.log(NameofMonth);
+
+
+
+//-------------------------------------
+//date number
+
+// var dayOfMonth = now.getDate();
+
+// console.log(dayOfMonth)
+
+
+//-------------------------------
+
+// var now = new Date();
+
+// var FullYear = now.getFullYear();
+
+// console.log(FullYear); //2026
+
+
+
+
+//----------------------------------
+
+// var DateNow = new Date();
+// console.log(DateNow);
+
+
+// var Hours = DateNow.getHours()
+// console.log(Hours); 
+
+
+// var Min = DateNow.getMinutes()
+// console.log(Min);
+
+// var Sec = DateNow.getSeconds()
+// console.log(Sec);
+
+
+// var Milisec = DateNow.getMilliseconds();
+// console.log(Milisec);
+
+
+
+// var DateNow = new Date();
+
+// var time = DateNow.getTime();
+// console.log(time);
+
+
+//-----------------------------------------------
+// date time difference 
+// var today = new Date(); 
+
+// var doomsday = new Date("June 30, 2035");  /// doom future
+
+// var msToday = today.getTime();
+
+// // console.log(msToday);
+
+// var msDoomsday = doomsday.getTime();
+
+// // console.log(msDoomsday);
+    
+// var msDiff = msDoomsday - msToday;
+
+// // console.log(msDiff);
+// // var msDiff = msDoomsday - msToday;
+
+// var msDiff = msDoomsday - msToday;
+// console.log(msDiff);
+
+
+// var dDiff = msDiff / (1000 * 60 * 60 * 24);
+
+// console.log(Math.floor(dDiff));
+
+
+// // -----------------------------------------------------------
+
+// var msDiff = new Date("June 30, 2035").getTime() - new Date().getTime();
+// var daysTillDoom = Math.floor(msDiff / (1000 * 60 * 60 * 24));
+
+// console.log(daysTillDoom)
+
+
+//----------------------------------------
+// set ---
+
+// var d = new Date();
+
+// var d = new Date("June 30, 2025");
+// // d.setFullYear(2001);
+
+// // d.setDate(11);
+
+// // d.setMonth(11);
+
+// // d.setHours (10)
+
+// d.setDate(10)
+
+
+// console.log(d);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
